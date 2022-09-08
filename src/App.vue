8f7ced30748304999998455b1f6f4233af6e8bd4
jiaokaihang <2096864475@qq.com>
2022-09-09 02:04:26 +0800

    底部初始完成

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
     <div class="docker">
-      <span class="docker__item">
-        <div class="logo">&#xe6b8;</div>
-        首页</span>
-      <span class="docker__item">
+      <div class="docker__item docker__item--active">
+        <div class="logo">&#xe6bb;</div>
+        <div docker__title>首页</div>
+      </div>
+      <div class="docker__item">
         <div class="logo">&#xe61b;</div>
-        购物车</span>
-      <span class="docker__item">
+        <div docker__title>购物车</div>
+      </div>
+      <div class="docker__item">
         <div class="logo">&#xe897;</div>
-        订单</span>
-      <span class="docker__item">
+        <div docker__title>订单</div>
+      </div>
+      <div class="docker__item">
         <div class="logo">&#xe8a0;</div>
-        我的</span>
+        <div docker__title>我的</div>
+      </div>
     </div>
   </div>
 </template>
@@ -24,7 +28,7 @@ export default {
 </script>
 
 <style lang="scss">
-.docker{
+.docker {
   display: flex;
   position: absolute;
   padding: 0 .18rem;
@@ -33,11 +37,26 @@ export default {
   bottom: 0;
   width: 100%;
   height: 0.49rem;
-  border-top:1px solid #F1F1F1 ;
-  background-color: #09f;
+  border-top: 1px solid #F1F1F1;
+
+  &__item {
+    flex: 1;
+    text-align: center;
+
+    .logo {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: .18rem
+    }
+
+    &--active {
+      color: #1FA4Fc;
+    }
+  }
+  &__title{
+    font-size: 20px;
+  transform: scale(.5 .5);
+  transform-origin: center top;
+  }
 }
-.docker__item{
-   flex: 1;
-   text-align: center;
-}
+
 </style>
