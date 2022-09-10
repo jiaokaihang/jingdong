bbafef5515b3d9dd4196196d64abb63092d1ea7f
jiaokaihang <2096864475@qq.com>
2022-09-10 17:57:43 +0800

    flex布局小图标完成

--- src/App.vue
+++ src/App.vue
@@ -7,32 +7,75 @@
         <span class="iconfont position_notice">&#xe613;</span>
       </div>
       <div class="search">
-       <span class="iconfont">&#xe6e1;</span>
-       <span class="search__text">山姆会员商店优惠商品</span>
-    </div>
-    <div class="banner" >
-      <img class="banner__img" src="banner.webp" alt="">
-    </div>
-    </div>
-    <div class="docker">
-      <div class=" docker__item docker__item--active">
-        <div class="iconfont">&#xe6bb;</div>
-        <div docker__title>首页</div>
+        <span class="iconfont">&#xe6e1;</span>
+        <span class="search__text">山姆会员商店优惠商品</span>
       </div>
-      <div class="docker__item">
-        <div class="iconfont">&#xe61b;</div>
-        <div docker__title>购物车</div>
+      <div class="banner">
+        <img class="banner__img" src="./assets/banner.webp" />
       </div>
-      <div class="docker__item">
-        <div class="iconfont">&#xe897;</div>
-        <div docker__title>订单</div>
+      <div class="icons">
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/医药健康.png" />
+          <p class="icons__item__desc">医药健康</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/大牌免运.png" />
+          <p class="icons__item__desc">大牌免运</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/家居.png" />
+          <p class="icons__item__desc">家居</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/水果店.png" />
+          <p class="icons__item__desc">水果店</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/签到.png" />
+          <p class="icons__item__desc">签到</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/红包.png" />
+          <p class="icons__item__desc">红包</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/菜市场.png" />
+          <p class="icons__item__desc">菜市场</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/蛋糕.png" />
+          <p class="icons__item__desc">蛋糕</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/超市.png" />
+          <p class="icons__item__desc">超市</p>
+        </div>
+        <div class="icons__item">
+          <img class="icons__item__img" src="./assets/鲜花.png" />
+          <p class="icons__item__desc">鲜花</p>
+        </div>
       </div>
-      <div class="docker__item">
-        <div class="iconfont">&#xe8a0;</div>
-        <div docker__title>我的</div>
-      </div>
+      <div class="gap"></div>
     </div>
-  </div>
+      <div class="docker">
+        <div class=" docker__item docker__item--active">
+          <div class="iconfont">&#xe6bb;</div>
+          <div docker__title>首页</div>
+        </div>
+        <div class="docker__item">
+          <div class="iconfont">&#xe61b;</div>
+          <div docker__title>购物车</div>
+        </div>
+        <div class="docker__item">
+          <div class="iconfont">&#xe897;</div>
+          <div docker__title>订单</div>
+        </div>
+        <div class="docker__item">
+          <div class="iconfont">&#xe8a0;</div>
+          <div docker__title>我的</div>
+        </div>
+      </div>
+      </div>
 </template>
 
 <script>
@@ -44,6 +87,7 @@ export default {
 <style lang="scss">
 @import './style/viriables.scss';
 @import './style/mixins.scss';
+
 .wrapper {
   position: absolute;
   left: 0;
@@ -52,12 +96,14 @@ export default {
   right: 0;
   padding: 0 .18rem;
 }
+
 .position {
   position: relative;
   padding: .16rem .24rem .16rem 0;
   line-height: .22rem;
   font-size: .16rem;
   @include ellipsis;
+
   .position__icon {
     font-size: .2rem;
     position: relative;
@@ -70,61 +116,87 @@ export default {
     top: .17rem;
     font-size: .2rem;
   }
-color:$content-fontcolor;
+
+  color:$content-fontcolor;
 }
+
 .search {
   margin-bottom: .12rem;
   line-height: .32rem;
   background: #F5F5F5;
   color: #B7B7B7;
   border-radius: .16rem;
+
   .iconfont {
     display: inline-block;
     padding: 0 .08rem 0 .16rem;
     font-size: .16rem;
   }
+
   &__text {
     display: inline-block;
     font-size: .14rem;
   }
 }
 
-.banner{
+.banner {
   height: 0;
   overflow: hidden;
   padding-bottom: 25.4%;
-  &__img{
+
+  &__img {
     width: 100%;
   }
 }
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: .16rem;
+  &__item{
+    width: 20%;
+    &__img {
+      display:block;
+      width: .4rem;
+      height: .4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      margin: .06rem 0 .16rem 0;
+      text-align: center;
+      color: $content-fontcolor;
+    }
+  }
+}
+.gap{
+  margin: 0 -.18rem;
+   height: .1rem;
+   background-color:$content-bgColor;
+}
 .docker {
   display: flex;
+  box-sizing: border-box;
   position: absolute;
   padding: 0 .18rem;
-  box-sizing: border-box;
   left: 0;
   bottom: 0;
   width: 100%;
-  height: 0.49rem;
-  border-top: 1px solid #F1F1F1;
+  height: .49rem;
+  border-top: .01rem solid $content-bgColor;
   color: $content-fontcolor;
   &__item {
     flex: 1;
     text-align: center;
-
     .iconfont {
-      margin: 0.07rem 0 0.02rem 0;
-      font-size: .18rem
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
     }
-
     &--active {
-      color: #1FA4Fc;
+      color: #1FA4FC;
     }
   }
-
   &__title {
-    font-size: 20px;
-    transform: scale(.5 .5);
+    font-size: .2rem;
+    transform: scale(.5, .5);
     transform-origin: center top;
   }
 }
