df088ebed7818006310eb7457dc8ca4a7c409455
jiaokaihang <2096864475@qq.com>
2022-09-11 15:40:33 +0800

    封装axios进行post请求

--- src/views/login/LoginList.vue
+++ src/views/login/LoginList.vue
@@ -16,10 +16,8 @@
 
 <script>
 import { useRouter } from 'vue-router'
-import axios from 'axios'
+import { post } from '../../utils/request'
 import { reactive } from '@vue/reactivity'
-
-axios.defaults.headers.post['Content-Type'] = 'application/json'
 
 export default {
   name: ' LoginList ',
@@ -29,16 +27,22 @@ export default {
       password: ''
     })
     const router = useRouter()
-    const handleLogin = () => {
-      axios.post('https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd/api/user/login', {
-        username: data.username,
-        password: data.password
-      }).then(() => {
-        localStorage.isLogin = true
-        router.push({ name: 'Home' })
-      }).catch(() => {
-        alert('登录失败')
-      })
+    const handleLogin = async () => {
+      try {
+        const result = await post('/api/user/login', {
+          username: data.username,
+          password: data.password
+        })
+        console.log(result)
+        if (result?.data?.errno === 0) {
+          localStorage.isLogin = true
+          router.push({ name: 'Home' })
+        } else {
+          alert('登录失败')
+        }
+      } catch (e) {
+        alert('请求失败')
+      }
     }
     const handleRegisterClick = () => {
       router.push({ name: 'Register' })
